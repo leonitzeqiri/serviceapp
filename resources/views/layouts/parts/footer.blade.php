2f4dda2fb5756df9604d2d4a2bddca77147793e5
Leonit <leonitzeqirii1@gmail.com>
2022-08-17 15:50:38 +0200

    'Added Layouts'

--- resources/views/layouts/parts/footer.blade.php
+++ resources/views/layouts/parts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-veryDarkBlue">
+<footer class="bg-veryDarkBlue fixed bottom-0 left-0 w-full">
     <div class="mx-12 px-12 py-10 mx-auto">
         <div class="hidden text-white md:block" style="text-align:center">
             Copyright &copy; 2022, All Rights Reserved
