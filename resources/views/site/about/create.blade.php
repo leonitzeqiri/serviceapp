79f9ef88d79e886bdcced2b0632ba44eb7e363e7
Leonit <leonitzeqirii1@gmail.com>
2022-08-23 23:03:14 +0200

    Revert "'fix:bug'"
    This reverts commit 057491992377301ba5d8fb70eb024de1ae68c2d9.

--- resources/views/site/about/create.blade.php
+++ resources/views/site/about/create.blade.php
@@ -33,22 +33,6 @@
         <label class="block uppercase tracking-wide     text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
             Description
         </label>
-        <select
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-            id="position_id" name="position_id">
-            @foreach ($positions as $position)
-                    <option value="{{ $position->id }}">{{ $position->position }}</option>
-            @endforeach
-            @error('position_id')
-                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-            @enderror
-        </select>
-    </div>
-
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <label class="block uppercase tracking-wide     text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-            Description
-        </label>
         <input
             class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             id="grid-first-name" name="description" type="text">
