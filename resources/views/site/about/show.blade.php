42256821d3baf6bb27005a29cf78c31ff503eeb9
Leonit <leonitzeqirii1@gmail.com>
2022-08-24 22:39:30 +0200

    'fix:views'

--- resources/views/site/about/show.blade.php
+++ resources/views/site/about/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="flex flex-col mt-24 md:flex-row md:space-x-6">
+<div class="flex flex-col ml-center ml-4 mt-24 md:flex-row md:space-x-6">
     <!-- Testimonial 1 -->
     <div
       class="flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3"
@@ -14,13 +14,15 @@
       </p>
     </div>
 </div>
+
 <form method="POST" action="{{ route('about.destroy', $about->id) }}">
     @csrf
 
     @method('DELETE')
     <div class="flex">
+        <a class="px-24 py-2 mt-4 ml-2 text-white bg-blue-900 rounded-lg hover:bg-red-500"href="{{ route('about.edit', $about->id) }}">Edit</a>
         <button type="submit"
-            class="px-24 py-2 mt-4 ml-2 text-white bg-blue-900 rounded-lg hover:bg-red-500">Delete</button>
+            class="px-24 py-2 mt-4 ml-2 text-white bg-red-900 rounded-lg hover:bg-red-500">Delete</button>
     </div>
-</form>
+
 @endsection
