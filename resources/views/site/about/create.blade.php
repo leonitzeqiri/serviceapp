e16442aa481108e396d3d158d3437a8a1e6bd6e8
Leonit <leonitzeqirii1@gmail.com>
2022-08-25 17:34:47 +0200

    'Added Resources'

--- resources/views/site/about/create.blade.php
+++ resources/views/site/about/create.blade.php
@@ -1,53 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-
     <form method="POST" action="{{ route('about.store') }}" enctype="multipart/form-data">
         @csrf
 
 
 
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-          File Upload
-        </label>
-        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" name="logo" type="file">
-        @error('logo')
-        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-    @enderror
-      </div>
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                File Upload
+            </label>
+            <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="grid-first-name" name="logo" type="file">
+            @error('logo')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+            @enderror
+        </div>
 
-      <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-            Name
-        </label>
-        <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-            id="grid-first-name" name="name" type="text">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                Name
+            </label>
+            <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="grid-first-name" name="name" type="text">
             @error('name')
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
             @enderror
-    </div>
+        </div>
 
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <label class="block uppercase tracking-wide     text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-            Description
-        </label>
-        <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-            id="grid-first-name" name="description" type="text">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide     text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+               Position
+            </label>
+            <select
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="position_id" name="position_id">
+                @foreach ($positions as $position)
+                    <option value="{{ $position->id }}">{{ $position->position }}</option>
+                @endforeach
+                @error('position_id')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </select>
+        </div>
+
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide     text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                Description
+            </label>
+            <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="grid-first-name" name="description" type="text">
             @error('description')
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
             @enderror
-    </div>
+        </div>
         <div class="mb-6">
             <div class="flex">
                 <button type="submit"
                     class="px-24 py-2 mt-4 ml-2 text-white bg-blue-900 rounded-lg hover:bg-red-500">Add</button>
             </div>
             <div class="flex">
-            <a href="/" class="px-24 py-2 mt-4 ml-2  text-white bg-red-900 rounded-lg hover:bg-red-500"> Back </a>
-        </div>
+                <a href="/" class="px-24 py-2 mt-4 ml-2  text-white bg-red-900 rounded-lg hover:bg-red-500"> Back </a>
+            </div>
     </form>
-
 @endsection
--- resources/views/site/about/show.blade.php
+++ resources/views/site/about/show.blade.php
@@ -2,13 +2,14 @@
 
 @section('content')
 
-<div class="flex flex-col mt-24 md:flex-row md:space-x-6">
+<div class="flex flex-col mt-24 ml-8 md:flex-row md:space-x-6">
     <!-- Testimonial 1 -->
     <div
       class="flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3"
     >
     <img src="{{ asset('storage/' . $about->logo) }}" class="w-16 -mt-14" alt="" />
       <h5 class="text-lg font-bold">{{ $about->name }}</h5>
+         <h1>{{ $about->position->position }}</h1>
       <p class="text-sm text-darkGrayishBlue">
        {{ $about->description }}
       </p>
@@ -18,9 +19,10 @@
     @csrf
 
     @method('DELETE')
-    <div class="flex">
+    <div class="flex ml-8">
+        <a class="px-24 py-2 mt-4 ml-2 text-white bg-blue-900 rounded-lg hover:bg-red-500"href="{{ route('about.edit', $about->id) }}">Edit</a>
         <button type="submit"
-            class="px-24 py-2 mt-4 ml-2 text-white bg-blue-900 rounded-lg hover:bg-red-500">Delete</button>
+            class="px-24 py-2 mt-4 ml-2 text-white bg-red-900 rounded-lg hover:bg-red-500">Delete</button>
     </div>
-</form>
+
 @endsection
