8eb251313f3d19f3a7a7b51127a09b22cc398e30
Leonit <leonitzeqirii1@gmail.com>
2022-08-25 17:57:18 +0200

    'fix:resources'

--- resources/views/site/about/create.blade.php
+++ resources/views/site/about/create.blade.php
@@ -29,10 +29,9 @@
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
             @enderror
         </div>
-
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label class="block uppercase tracking-wide     text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-               Position
+                Position
             </label>
             <select
                 class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
@@ -57,6 +56,7 @@
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
             @enderror
         </div>
+
         <div class="mb-6">
             <div class="flex">
                 <button type="submit"
