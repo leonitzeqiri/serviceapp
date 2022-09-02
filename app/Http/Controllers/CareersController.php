bde73fe888581a8db3bb1b4449b3ce1146962ea6
Leonit Zeqiri <leonitzeqirii1@gmail.com>
2022-09-02 23:53:26 +0200

    'Added Careers'

--- app/Http/Controllers/CareersController.php
+++ app/Http/Controllers/CareersController.php
@@ -3,14 +3,40 @@
 namespace App\Http\Controllers;
 
 use Exception;
-use Illuminate\Http\Request;
+use App\Http\Requests\JobRequest;
+use App\Models\Careers;
 
 class CareersController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         try {
-        return view('site.careers.index');
-        }catch(Exception $e){
+            $careers = Careers::latest();
+            return view('site.careers.index', ['careers' => $careers]);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    public function show()
+    {
+    }
+
+    public function create()
+    {
+        try {
+            return view('site.careers.create');
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    public function store(JobRequest $request)
+    {
+        try {
+            Careers::create($request->validated());
+            return redirect('/careers')->with('message', 'Jobs Listing Succesfully');
+        } catch (Exception $e) {
             throw new Exception($e->getMessage());
         }
     }
