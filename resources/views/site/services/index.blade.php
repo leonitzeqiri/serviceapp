adae239e6fb009db81c64290a5d15e80ab73ee5a
Leonit Zeqiri <leonitzeqirii1@gmail.com>
2022-09-21 22:47:52 +0200

    'fix:resources'

--- resources/views/site/services/index.blade.php
+++ resources/views/site/services/index.blade.php
@@ -31,27 +31,29 @@
 
 
             <section id="cta" class="bg-brightRed">
-        <div
-            class="container flex flex-col items-center justify-between  mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0"style="padding-top:200px">
-            <div class="flex flex-col mb-32  ml-8 space-y-12 md:w-1/2">
-                <h1 class="max-w-md text-4xl font-bold text-center md:text-2xl md:text-left">
-                    Bring everyone together to build better products
-                </h1>
-                <p class="max-w-sm text-center text-darkGrayishBlue md:text-left">
-                    Manage makes it simple for software teams to plan day-to-day tasks
-                    while keeping the larger team goals in view.
-                </p>
-                <div class="flex justify-center md:justify-start">
-                    <a href="#"
+                @foreach ($collaborate as $collaborate)
+
+                <div
+                class="container flex flex-col items-center justify-between  mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0"style="padding-top:100px">
+                <div class="flex flex-col mb-32  ml-8 space-y-12 md:w-1/2">
+                    <h1 class="max-w-md text-4xl font-bold text-center md:text-2xl md:text-left">
+                        {{ $collaborate->title }}
+                    </h1>
+                    <p class="max-w-sm text-center text-darkGrayishBlue md:text-left">
+                        {{ $collaborate->description }}
+                    </p>
+                    <div class="flex justify-center md:justify-start">
+                        <a href="#"
                         class="p-3 px-6 pt-2 text-white bg-veryDarkBlue rounded-full baseline hover:bg-veryDarkBlue">Get
                         Started</a>
+                    </div>
+                </div>
+                <!-- Image -->
+                <div class="md:w-1/2 pt-4">
+                    <img src="{{ asset('storage/' . $collaborate->image) }}" alt="" />
                 </div>
             </div>
-            <!-- Image -->
-            <div class="md:w-1/2">
-                <img src="images/illustration-intro.svg" alt="" />
-            </div>
-        </div>
+        @endforeach
 
     </section>
 
