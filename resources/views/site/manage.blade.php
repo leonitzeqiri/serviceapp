55d58728df940690b115f75cfc605848ddfa83a8
Leonit Zeqiri <leonitzeqirii1@gmail.com>
2022-10-03 23:46:57 +0200

    'added'

--- resources/views/site/manage.blade.php
+++ resources/views/site/manage.blade.php
@@ -3,30 +3,29 @@
 @section('content')
     <table class="w-full table-auto rounded-sm">
         <tbody>
-                @foreach ($services as $service)
-                    <tr class="border-gray-300">
-                        <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                            <a href="show.html">
-                                {{ $service->title }}
-                            </a>
-                        </td>
-                        <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                            <a href="" class="text-blue-400 px-6 py-2 rounded-xl"><i
-                                    class="fa-solid fa-pen-to-square"></i>
-                                Edit</a>
-                        </td>
-                        <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                            <form method="POST" action="">
-                                @csrf
-                                @method('DELETE')
-                                <button class="text-red-600">
-                                    <i class="fa-solid fa-trash-can"></i>
-                                    Delete
-                                </button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
+            @foreach ($services as $service)
+                <tr class="border-gray-300">
+                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                        <a href="show.html">
+                            {{ $service->title }}
+                        </a>
+                    </td>
+                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                        <a href="" class="text-blue-400 px-6 py-2 rounded-xl"><i class="fa-solid fa-pen-to-square"></i>
+                            Edit</a>
+                    </td>
+                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                        <form method="POST" action="">
+                            @csrf
+                            @method('DELETE')
+                            <button class="text-red-600">
+                                <i class="fa-solid fa-trash-can"></i>
+                                Delete
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
 
         <tbody>
@@ -38,8 +37,7 @@
                         </a>
                     </td>
                     <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                        <a href="" class="text-blue-400 px-6 py-2 rounded-xl"><i
-                                class="fa-solid fa-pen-to-square">
+                        <a href="" class="text-blue-400 px-6 py-2 rounded-xl"><i class="fa-solid fa-pen-to-square">
                             </i>
                             Edit</a>
                     </td>
@@ -55,8 +53,33 @@
                     </td>
                 </tr>
             @endforeach
-    </tbody>
+        </tbody>
+
+        <tbody>
+            @foreach ($collaborate as $collaborate)
+                <tr class="border-gray-300">
+                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                        <a href="show.html">
+                            {{ $collaborate->title }}
+                        </a>
+                    </td>
+                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                        <a href="" class="text-blue-400 px-6 py-2 rounded-xl"><i class="fa-solid fa-pen-to-square">
+                            </i>
+                            Edit</a>
+                    </td>
+                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                        <form method="POST" action="">
+                            @csrf
+                            @method('DELETE')
+                            <button class="text-red-600">
+                                <i class="fa-solid fa-trash-can"></i>
+                                Delete
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
     </table>
-
-
 @endsection
